def442e9fcaa6bbc8f1c3aabce6a69ec0a0f87d6
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-28 21:54:15 +0530

    Add files via upload

---
+++ _21ex.java
@@ -0,0 +1,16 @@
+class _21ex 
+{    // Main driver method
+        public static void main(String args[])
+        {
+     
+            // Input character and string
+            char a = 's';
+            String str = "iamlearninglanguage";
+     
+            // Inserting at the end
+            String str2 = str + a;
+     
+            // Print and display the above string
+            System.out.println(str2);
+        }
+    }
